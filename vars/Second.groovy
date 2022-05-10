097ac436e85e8d87454239b2562e3a456dac091e
hnimmagadda1 <gottipati.elite@gmail.com>
2022-05-10 12:23:29 +0530

    added

--- vars/Second.groovy
+++ vars/Second.groovy
@@ -1,9 +1,3 @@
 def call(){
-    //def PIPELINE_PROPS = readProperties file: 'properties/pipeline.properties'
-    gitCheckOut("https://github.com/arnaud-deprez/demo-jenkins-pipeline-gradle.git", "master", "First")
-
-
-       // compileSource("${PIPELINE_PROPS["PROJECT_NAME"]}",
-          //      "${PIPELINE_PROPS["BUILD_COMMAND"]}",
-           //     "${PIPELINE_PROPS["CLASSIFIER"]}", "${PIPELINE_PROPS["GRADLE_VERSION"]}")
+    sh './gradlew clean build'
 }
--- vars/gitCheckOut.groovy
+++
@@ -1,7 +0,0 @@
-def call(final String ScmURL, String BranchName, String DirName) {
-    checkout([$class           : 'GitSCM',
-              branches         : [[name: "$BranchName"]],
-              extensions       : [[$class: 'CloneOption', timeout: 60,]],
-              //extensions       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: First + "$DirName" + "${env.JOB_BASE_NAME}"]],
-              userRemoteConfigs: [url: "$ScmURL"]])
-}
