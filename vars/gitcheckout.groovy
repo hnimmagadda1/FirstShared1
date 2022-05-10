9b6c31061b1a769f478997011c19d8d651210c6b
hnimmagadda1 <gottipati.elite@gmail.com>
2022-05-10 17:49:14 +0530

    added

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,4 +1,5 @@
 def call(){
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/arnaud-deprez/demo-jenkins-pipeline-gradle.git']]])
-    sh './gradlew clean build'
+    //sh './gradlew clean build'
+    build 'demo-jenkins-pipeline-gradle'
 }
