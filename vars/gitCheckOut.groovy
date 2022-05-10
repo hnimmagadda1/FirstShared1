77a8838467b05ecdc472ffde301ed32d2ef5d346
hnimmagadda1 <gottipati.elite@gmail.com>
2022-05-10 11:54:45 +0530

    added

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -2,6 +2,6 @@ def call(final String ScmURL, String BranchName, String DirName) {
     checkout([$class           : 'GitSCM',
               branches         : [[name: "$BranchName"]],
               extensions       : [[$class: 'CloneOption', timeout: 60,]],
-              extensions       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: CUSTOM_WORKSPACE + "$DirName" + "${env.JOB_BASE_NAME}"]],
+              //extensions       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: First + "$DirName" + "${env.JOB_BASE_NAME}"]],
               userRemoteConfigs: [[credentialsId: None, url: "$ScmURL"]]])
 }
