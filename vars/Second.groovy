4d68931420bc23b460ed41862b51b0fcbbfa088d
hnimmagadda1 <gottipati.elite@gmail.com>
2022-05-10 10:54:19 +0530

    added

--- vars/Second.groovy
+++ vars/Second.groovy
@@ -1,4 +1,5 @@
 def call(){
     ["git", "clone", "https://github.com/arnaud-deprez/demo-jenkins-pipeline-gradle.git",  "/Baseline/Package/Logistics/"].execute()
+    checkout()
     echo "Cloned"
 }
